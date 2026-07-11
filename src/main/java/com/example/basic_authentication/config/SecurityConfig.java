1129b2440fa141eadce2b0670967bb9f7475e43c
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-11 18:28:15 +0200

    fix bean PasswordEncoder

--- src/main/java/com/example/basic_authentication/config/SecurityConfig.java
+++ src/main/java/com/example/basic_authentication/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public static PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
