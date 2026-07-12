937b872a813a7704d4b166e33a34a3b3a0f48573
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-12 15:13:09 +0200

    add configurations for password hashing and Inmemory User objects

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +24,32 @@ public class SecurityConfig {
 
         return http.build();
 
+    }
+
+    //Creating in-memory objects and stores them at InMemoryDetailsManager object
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails lebogang = User.builder()
+                .username("lebogang")
+                .password(passwordEncoder().encode("lebogang"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails accaria = User.builder()
+                .username("accartia")
+                .password(passwordEncoder().encode("accartia"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(lebogang,accaria);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
